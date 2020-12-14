8958d439fb94234b14c81ab11cef12e2a0be8255
Damaris <44580236+dama207@users.noreply.github.com>
2020-12-14 10:09:37 -0500

    Add files via upload

--- data/process_data.py
+++ data/process_data.py
@@ -5,7 +5,16 @@ from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
     
-    '''Load data of messages and categories and marge them on only dataframe'''
+    '''Load data of messages and categories and marge them on only dataframe
+    Args:
+        messages_filepath : Path file messages.
+        categories_filepath : Path file categories.
+        
+    Returns:
+    
+        df: Dataframe with data merged
+        
+    '''
     # load messages dataset
     messages = pd.read_csv(messages_filepath)
     # load categories dataset
@@ -18,7 +27,13 @@ def load_data(messages_filepath, categories_filepath):
 
 def clean_data(df):
     
-    '''Clean dataframe and build a feature for each category, convert text in categories in 1 or 0 for each category, delete duplicates'''
+    '''Clean dataframe and build a feature for each category, convert text in categories in 1 or 0 for each category, delete duplicates
+    Args:
+        df: Dataframe
+    Returns:
+        df: Dataframe clean with column for each category with binary values
+    
+    '''
     # create a dataframe of the 36 individual category columns
     categories = df.join(df.categories.str.split(';',expand=True))
     categories=categories.drop(columns= ['categories','message','original','genre'])
@@ -32,6 +47,8 @@ def clean_data(df):
     for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].apply(lambda x : int((re.sub("\D", "",x))))
+        # replace 2 to 1 
+        categories[column].replace(2, 1, inplace=True)
     
     categories.reset_index(inplace=True)
     # drop the original categories column from `df`
@@ -46,7 +63,11 @@ def clean_data(df):
 
 def save_data(df, database_filename):
     
-    '''save data into base'''
+    '''save data into base
+    Args:
+       df : dataframe
+       database_filename : name database   
+    '''
     engine = create_engine('sqlite:///'+ database_filename)
     df.to_sql('categorias_base', engine, index=False)
     
